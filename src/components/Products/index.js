d8123514e65db60f37c447157f7530983cf3c157
kameswari <vepadakameswari622@gmail.com>
2023-10-14 03:42:32 +0000

    solutions

---
+++ src/components/Products/index.js
@@ -0,0 +1,18 @@
+import Header from '../Header'
+import './index.css'
+
+const Products = () => (
+  <div>
+    <Header />
+
+    <div className="container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png"
+        alt="products"
+        className="image1"
+      />
+    </div>
+  </div>
+)
+
+export default Products
